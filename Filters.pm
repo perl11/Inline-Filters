8875b75052b85c95ad2916c498aef967e2a6b29d
Reini Urban <rurban@cpanel.net>
2017-01-26 08:53:53 -0600

    Release 0.20
    added LICENSE => perl_5 to meta, which will now warn with wrong
    "Invalid LICENSE value 'perl_5' ignored" on older EUMM/CPAN::Meta::Spec
    installations. Ignore it, it's valid.
    
    See [cpan #75344] and [cpan #98298] on EUMM.
    perldoc CPAN::Meta::Spec

--- Filters.pm
+++ Filters.pm
@@ -1,7 +1,7 @@
 package Inline::Filters;
 use strict;
 use Config;
-our $VERSION = "0.19";
+our $VERSION = "0.20";
 use File::Spec;
  
 #============================================================================
